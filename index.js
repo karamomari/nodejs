4d4c31dfeb4ae44f319cc340a8916feed1a56a98
karamomari <karamomari20010@gmail.com>
2025-10-06 16:34:55 +0300

    DS

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const app = express();
+const PORT = 3000;
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Hello Backend!");
+});
+
+
+app.get("/hello", (req, res) => {
+  res.send("Hello Backend!");
+});
+
+
+
+
+
+// تشغيل السيرفر
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
